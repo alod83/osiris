5dbf730ca28d298c3763273147a7ba7de49333e8
Angelica Lo Duca <angelica.loduca@iit.cnr.it>
2021-10-23 22:31:00 +0200

    Update test.py

--- osiris2/data analysis/test.py
+++ osiris2/data analysis/test.py
@@ -1,44 +1,69 @@
 import pandas as pd
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import GridSearchCV
 import numpy as np
 import pickle
 from sklearn.metrics import precision_score, recall_score, accuracy_score
 
+import sys, getopt
 
-#df = pd.read_csv('source/dataset_60_balanced.csv')
-print('Reading Datasets')
-X_test = pd.read_csv('output/X_train_60.csv')
-y_test = pd.read_csv('output/y_train_60.csv')
-print('Done')
-columns = ['class', 'course','speed','row','column','hour_sin','hour_cos','day_sin','day_cos']
-#X = df[columns]
-#Y = df['target']
+def help_and_exit():
+    print('test.py -m <model_name> -d <train/test>')
+    sys.exit(2)
 
-X_test = X_test[columns]
-y_test = y_test['0']
+def main(argv):
+    model = ''
+    dataset = ''
+    try:
+        opts, args = getopt.getopt(argv,"hm:d:",["model=","dataset="])
+    except getopt.GetoptError:
+        help_and_exit()
+    for opt, arg in opts:
+        if opt == '-h':
+            help_and_exit()
+        elif opt in ("-m", "--model"):
+            model = arg
+        elif opt in ("-d", "--dataset"):
+            dataset = arg
+    print('Model is ' + model)
+    print('Dataset is ' + dataset)
 
-import matplotlib.pyplot as plt
-from sklearn.metrics import roc_curve
+    print('Reading Datasets')
+    X_test = pd.read_csv('output/X_'+ dataset + '_60.csv')
+    y_test = pd.read_csv('output/y_'+ dataset + '_60.csv')
+    print('Done')
+    columns = ['class', 'course','speed','row','column','hour_sin','hour_cos','day_sin','day_cos']
+    #X = df[columns]
+    #Y = df['target']
 
-# open a file, where you stored the pickled data
-print('Open Model')
-file = open('output/dt.sav', 'rb')
+    X_test = X_test[columns]
+    y_test = y_test['0']
 
-# dump information to that file
-loaded_model = pickle.load(file)
-print('Done')
+    import matplotlib.pyplot as plt
+    from sklearn.metrics import roc_curve
 
-# In[133]:
+    # open a file, where you stored the pickled data
+    print('Open Model')
+    file = open('output/' + model + '.sav', 'rb')
+
+    # dump information to that file
+    loaded_model = pickle.load(file)
+    print('Done')
+
+    # In[133]:
 
 
 
-y_pred = loaded_model.predict(X_test)
-precision = precision_score(y_test.values.ravel(),y_pred, average='macro')
-recall = recall_score(y_test.values.ravel(),y_pred, average='macro')
-accuracy = accuracy_score(y_test.values.ravel(),y_pred)
-print('Precision: ' , precision)
-print('Recall: ' , recall)
-print('Accuracy: ' , accuracy)
+    y_pred = loaded_model.predict(X_test)
+    precision = precision_score(y_test.values.ravel(),y_pred, average='macro')
+    recall = recall_score(y_test.values.ravel(),y_pred, average='macro')
+    accuracy = accuracy_score(y_test.values.ravel(),y_pred)
+    print('Precision: ' , precision)
+    print('Recall: ' , recall)
+    print('Accuracy: ' , accuracy)
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+   
 
 
