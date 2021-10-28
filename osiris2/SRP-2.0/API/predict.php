3624dc9894e2c1c3c18667c9b6c3d4345272c0cf
Angelica Lo Duca <angelica.loduca@iit.cnr.it>
2021-10-28 11:31:03 +0200

    Update predict.php

--- osiris2/SRP-2.0/API/predict.php
+++ osiris2/SRP-2.0/API/predict.php
@@ -3,30 +3,24 @@
 $python_env = "/usr/bin/python3";
 header('Content-Type: application/json');
 
-if(isset($_GET['latitude']) && 
+/*if(isset($_GET['latitude']) && 
 		isset($_GET['longitude']) && 
 		isset($_GET['heading']) && 
 		isset($_GET['sog']) && 
 		isset($_GET['basic_class']) &&
-		isset($_GET['date']))
-{
-	$lat = $_GET['latitude'];
-	$lng = $_GET['longitude'];
-	$heading = $_GET['heading'];
-	$sog = $_GET['sog'];
-	$bc = $_GET['basic_class']; 
-	$mydate = str_replace("T", " ", $_GET['date']);
+		isset($_GET['date']))*/
+//{
+	$lat = isset($_GET['latitude']) ? $_GET['latitude'] : 37.23098;
+	$lng = isset($_GET['longitude']) ? $_GET['longitude'] : 12.68286;
+	$heading = isset($_GET['heading']) ? $_GET['heading'] : 46;
+	$sog = isset($_GET['sog']) ? $_GET['sog'] : 13;
+	$bc = isset($_GET['basic_class']) ? $_GET['basic_class'] : 1; 
+	$mydate = isset($_GET['date']) ? str_replace("T", " ", $_GET['date']) : '2021-10-27 12:33:10';
 	$output = "";
 	exec("$python_env ../CLI/predict.py -l $lat -n $lng -c $heading -s $sog -b $bc -t $mydate", $output);
 	echo $output[0];
-}
-else if (isset($_GET['record_id']))
-{
-	$output = "";
-	$record_id = $_GET['record_id'];
-	exec("$python_env predict.py record_id -r $record_id", $output);
-	echo $output[0];
-}
-else
-	echo "Parameters: latitude, longitude, sog, heading, basic_class, date, timestamp";
+//}
+
+//else
+//	echo "Parameters: latitude, longitude, sog, heading, basic_class, date, timestamp";
 ?>
