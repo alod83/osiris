c183b30ece4ea9a408d23cd5a60dcc6b9a78b969
Angelica Lo Duca <angelica.loduca@iit.cnr.it>
2021-11-30 11:24:22 +0100

    updated build next status

--- osiris2/srp_pipeline/6_Build_Next_Status.sh
+++ osiris2/srp_pipeline/6_Build_Next_Status.sh
@@ -5,16 +5,18 @@ routes=($(cat resources/routes_names.txt))
 n_routes=${#routes[@]}
 burst=$((n_routes/n_cores))
 
-start=3540 # first 3539 already done
+start=0 # first 3539 already done
 for ((current=0;current<n_routes;current=current+burst));
 do
     echo $start
     current_routes=${routes[@]:$start:$burst}
-    for route in $current_routes;
-    do
-	    printf "Route $route\n"
-	    python3 build_next_status.py -r $route &
-    done
+	echo $current_routes > resources/routes_names_${start}.txt
+	./build_next_status_burst.sh $start &
+    #for route in $current_routes;
+    #do
+	#    printf "Route $route\n"
+	#    python3 build_next_status.py -r $route
+    #done
     start=$((current + burst))
 done
 
