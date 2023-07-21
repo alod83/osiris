e5a4a8494ab598934a75e977eedaee605f7244bd
Angelica Lo Duca <angelica.loduca@iit.cnr.it>
2023-07-21 16:14:27 +0200

    Delete osiris1 directory

--- osiris1/manage/run_setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-python /home/angelica/Git/osiris/srp/setup.py -s 12850257 -e 21486498 -b 1
-
--- osiris1/srp/test_date.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-#today = datetime.datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
-#print today.timetuple().tm_yday
-
-now = datetime.datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
-midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
-minutes = (now - midnight).seconds/60
-print minutes
